7fd30386292061fc212de6369053fc4b2385d79b
unknown <karlcalano@gmail.com>
2022-08-26 08:28:52 +0800

    adding src file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={`${process.env.PUBLIC_URL ?? ''}/cklogo.png`}
+          className="App-logo"
+          alt="logo"
+        />
+        <Counter />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
